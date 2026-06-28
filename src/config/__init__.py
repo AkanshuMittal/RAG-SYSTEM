6e7d28e03df1adea6e5ba336adee213ccc463239
AkanshuMittal <akanshumittal150@gmail.com>
2026-06-28 18:15:56 +0530

    packaging the config folder

---
+++ src/config/__init__.py
@@ -0,0 +1,7 @@
+import src.config.settings as _settings_module
+
+Settings = _settings_module.Settings
+get_settings = _settings_module.get_settings
+settings = _settings_module.settings
+
+__all__ = ["Settings", "get_settings", "settings"]
